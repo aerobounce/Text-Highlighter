0a81193c70a253a6a8b2b1a7574d877636a45048
aerobounce <10491362+aerobounce@users.noreply.github.com>
2022-05-25 21:21:46 +0900

    Migrate to Python 3.8
    - Add typings
    - Add Nil-checks
    - Suppress console errors

--- highlighter.py
+++ highlighter.py
@@ -1,136 +1,121 @@
-import sublime
-import sublime_plugin
-
-import re
 from collections import OrderedDict
+from re import escape
+from typing import List
+from typing import OrderedDict as tOrderedDict
 
-class TextHighlighterToggleCommand(sublime_plugin.WindowCommand):
-  # TODO: run this command when new tab is opened
-  def run(self):
-    active_view = self.window.active_view()
-    selected_region = active_view.sel()
-    sel_string = active_view.substr(selected_region[0])
+from sublime import DRAW_NO_OUTLINE, Region, View
+from sublime_plugin import EventListener, WindowCommand
 
-    # Get word on cursor if any region isn't selected.
-    if not sel_string:
-      word_region = active_view.word(selected_region[0])
-      sel_string = active_view.substr(word_region)
+COLORS_BY_SCOPE: tOrderedDict[str, str] = OrderedDict()
 
-    views = self.window.views()
-    if is_highlighted(self.window, sel_string):
-      color = find_used_color(self.window, sel_string)
-      for view in views:
-        eraser(view, sel_string, color)
-    else:
-      color = find_usable_color(self.window, sel_string)
-      for view in views:
-        highlighter(view, sel_string, color)
-
-
-class TextHighlighterClearAllCommand(sublime_plugin.WindowCommand):
-  def run(self):
-    window =self.window
-    views = self.window.views()
-
-    for sel_string in colors_by_scope.values():
-      if sel_string:
-        color = find_used_color(window, sel_string)
-        if color:
-          for view in views:
-              eraser(view, sel_string, color)
-
-
-class HighlighterCommand(sublime_plugin.EventListener):
-  def on_activated(self, view):
-    highlightAll(view)
-
-  def on_modified(self, view):
-    highlightAll(view)
-
-def highlightAll(view):
-  window = view.window()
-  views = window.views()
-
-  for view in views:
-    for color, sel_string in colors_by_scope.items():
-      if color and sel_string:
-        highlighter(view, sel_string, color)
-
-def highlighter(view, sel_string, color):
-  global colors_by_scope
-  window = view.window()
-
-  regions = find_all(view, sel_string)
-  if color and regions:
-    if not colors_by_scope[color]:
-      colors_by_scope[color] = sel_string
-
-    view.add_regions(
-      sel_string,
-      regions,
-      color,
-      'dot',
-      sublime.DRAW_NO_OUTLINE
-      )
-
-def eraser(view, sel_string, color):
-  global colors_by_scope
-  window = view.window()
-
-  regions = find_all(view, sel_string)
-  colors_by_scope[color] = None
-
-  view.erase_regions(sel_string)
-
-def find_all(view, sel_string):
-  return view.find_all(re.escape(sel_string))
-
-def is_highlighted(window, sel_string):
-  global colors_by_scope
-
-  highlighted = False
-  for key, value in colors_by_scope.items():
-    if value == sel_string:
-      highlighted = True
-      break
-  return highlighted
-
-def find_used_color(window, sel_string):
-  global colors_by_scope
-
-  color = None
-  for key, value in colors_by_scope.items():
-    if value == sel_string:
-      color = key
-  return color
-
-def find_usable_color(window, sel_string):
-  global colors_by_scope
-
-  color = None
-  for key, value in colors_by_scope.items():
-    if not value:
-      color = key
-      break
-  return color
 
 def plugin_loaded():
-  global colors_by_scope
+    COLORS_BY_SCOPE["markup.changed.git_gutter"] = ""  # vivid purple
+    COLORS_BY_SCOPE["support.class"] = ""  # yellow
+    COLORS_BY_SCOPE["markup.deleted.git_gutter"] = ""  # vivid pink
+    COLORS_BY_SCOPE["markup.inserted.git_gutter"] = ""  # vivid green
+    COLORS_BY_SCOPE["constant.numeric"] = ""  # orange
+    COLORS_BY_SCOPE["constant.character.escape"] = ""  # light blue
+    COLORS_BY_SCOPE["variable"] = ""  # red
+    COLORS_BY_SCOPE["string"] = ""  # light green
+    COLORS_BY_SCOPE["comment"] = ""  # glay
 
-  if 'colors_by_scope' in globals(): # Return colors_by_scope if colors_by_scope is already exists.
-    return colors_by_scope
 
-  colors_by_scope = OrderedDict() # TODO: Make color configurable
-  colors_by_scope['markup.changed.git_gutter'] = None # vivid purple
-  colors_by_scope['support.class'] = None # yellow
-  colors_by_scope['markup.deleted.git_gutter'] = None # vivid pink
-  colors_by_scope['markup.inserted.git_gutter'] = None # vivid green
-  colors_by_scope['constant.numeric'] = None # orange
-  colors_by_scope['constant.character.escape'] = None # light blue
-  colors_by_scope['variable'] = None # red
-  colors_by_scope['string'] = None # light green
-  colors_by_scope['comment'] = None # glay
+def plugin_unloaded():
+    COLORS_BY_SCOPE.clear()
 
-  return colors_by_scope
 
-plugin_loaded()
+class TextHighlighterToggleCommand(WindowCommand):
+    def run(self):  # pyright: ignore
+        if active_view := self.window.active_view():
+            selected_region = active_view.sel()
+            selection = active_view.substr(selected_region[0])
+
+            # Get word on cursor if any region isn't selected.
+            if not selection:
+                word_region = active_view.word(selected_region[0])
+                selection = active_view.substr(word_region)
+
+            views = self.window.views()
+            if is_highlighted(selection):
+                if color := find_used_color(selection):
+                    for view in views:
+                        eraser(view, selection, color)
+            else:
+                if color := find_usable_color():
+                    for view in views:
+                        highlighter(view, selection, color)
+
+
+class TextHighlighterClearAllCommand(WindowCommand):
+    def run(self):  # pyright: ignore
+        views = self.window.views()
+
+        for selection in COLORS_BY_SCOPE.values():
+            if selection:
+                color = find_used_color(selection)
+                if color:
+                    for view in views:
+                        eraser(view, selection, color)
+
+
+class HighlighterCommand(EventListener):
+    def on_activated(self, view: View):
+        highlightAll(view)
+
+    def on_modified(self, view: View):
+        highlightAll(view)
+
+
+def highlightAll(view: View):
+    if window := view.window():
+        views = window.views()
+
+        for view in views:
+            for color, selection in COLORS_BY_SCOPE.items():
+                if color and selection:
+                    highlighter(view, selection, color)
+
+
+def highlighter(view: View, selection: str, color: str):
+    regions = find_all(view, selection)
+    if color and regions:
+        if not COLORS_BY_SCOPE[color]:
+            COLORS_BY_SCOPE[color] = selection
+
+        view.add_regions(selection, regions, color, "dot", DRAW_NO_OUTLINE)
+
+
+def eraser(view: View, selection: str, color: str):
+    COLORS_BY_SCOPE[color] = ""
+    view.erase_regions(selection)
+
+
+def find_all(view: View, selection: str) -> List[Region]:
+    return view.find_all(escape(selection))
+
+
+def is_highlighted(selection: str):
+    highlighted = False
+    for _, value in COLORS_BY_SCOPE.items():
+        if value == selection:
+            highlighted = True
+            break
+    return highlighted
+
+
+def find_used_color(selection: str):
+    color = None
+    for key, value in COLORS_BY_SCOPE.items():
+        if value == selection:
+            color = key
+    return color
+
+
+def find_usable_color():
+    color = None
+    for key, value in COLORS_BY_SCOPE.items():
+        if not value:
+            color = key
+            break
+    return color
